6b4c25a4a3a33468063389276a19ec3e64da43ac
Tony Bowden <tony@tmtm.com>
2018-03-06 10:59:16 +0000

    Also get via Polish category

--- scraper.rb
+++ scraper.rb
@@ -11,29 +11,31 @@ term_2016 = EveryPolitician::Wikidata.wikipedia_xpath(
 term_2012 = EveryPolitician::Wikidata.wikipedia_xpath( 
   url:   'https://sk.wikipedia.org/wiki/Zoznam_poslancov_NR_SR_(2012_–_2016)',
   xpath: '//h2[contains(.,"Poslanci")]//following-sibling::ol[1]/li//a[not(@class="new")]/@title',
-) 
+)
 
 term_2010 = EveryPolitician::Wikidata.wikipedia_xpath( 
   url:   'https://sk.wikipedia.org/wiki/Zoznam_poslancov_NR_SR_(2010_–_2012)',
   xpath: '//h2[contains(.,"Poslanci")]//following-sibling::ol[1]/li//a[not(@class="new")]/@title',
-) 
+)
 
 term_2006 = EveryPolitician::Wikidata.wikipedia_xpath( 
   url:   'https://sk.wikipedia.org/wiki/Zoznam_poslancov_NR_SR_(2006_–_2010)',
   xpath: '//h2[contains(.,"Poslanci")]//following-sibling::ol[1]/li//a[1][not(@class="new")]/@title',
-) 
+)
 
 term_2002 = EveryPolitician::Wikidata.wikipedia_xpath(
   url:   'https://sk.wikipedia.org/wiki/Zoznam_poslancov_NR_SR_(2002_–_2006)',
   xpath: '//h2[contains(.,"Poslanci")]//following-sibling::ul[1]/li//a[1][not(@class="new")]/@title',
-) 
+)
 
 term_1998 = EveryPolitician::Wikidata.wikipedia_xpath( 
   url:   'https://sk.wikipedia.org/wiki/Zoznam_poslancov_NR_SR_(1998_–_2002)',
   xpath: '//h2[contains(.,"Poslanci")]//following-sibling::ul[1]/li//a[1][not(@class="new")]/@title',
-) 
+)
 
 names = (term_2016 + term_2012 + term_2010 + term_2006 + term_2002 + term_1998).uniq
 
-EveryPolitician::Wikidata.scrape_wikidata(names: { sk: names }, output: false) 
+plcat = WikiData::Category.new('Kategoria:Słowaccy parlamentarzyści', 'pl').member_titles
+
+EveryPolitician::Wikidata.scrape_wikidata(names: { sk: names, pl: plcat })
 
